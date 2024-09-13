8cb8ae3a617e64016a52b9b3f022013350c0188f
Min Kyu Kim <uxchampaign@gmail.com>
2024-09-13 13:43:17 +0900

    2024-09-13 problems

---
+++ src/week1_bruteforce_recursion/baek_17626.java
@@ -0,0 +1,28 @@
+package week1_bruteforce_recursion;
+
+import java.util.Scanner;
+
+public class baek_17626 {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n + 1];
+
+        arr[0] = 0;
+        arr[1] = 1;
+
+        for(int i = 2; i <= n; i++) {
+            int min = 5;
+            for(int j = 1; j * j <= i; j++) {
+                min = Math.min(min, arr[i - j*j]);
+            }
+            arr[i] = min + 1;
+        }
+
+        System.out.println(arr[n]);
+
+    }
+
+}
